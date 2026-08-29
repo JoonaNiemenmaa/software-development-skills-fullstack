b8f7ce2299e232dce999cc69ac38f4cc40ee85eb
Joona Niemenmaa <joonaoskari@hotmail.com>
2026-08-29 18:24:18 +0300

    frontend luotu

---
+++ project/frontend/src/pages/Auth.jsx
@@ -0,0 +1,15 @@
+const Auth = () => {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input name="username" type="text"></input>
+
+      <label htmlFor="username">Password</label>
+      <input name="password" type="password"></input>
+
+      <button>Login</button>
+    </form>
+  )
+}
+
+export default Auth;
