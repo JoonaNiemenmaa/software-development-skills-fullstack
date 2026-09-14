dbbfbdef8d426e9ced1670b5f3d9b66edebf5b65
Joona Niemenmaa <joonaoskari@hotmail.com>
2026-09-14 20:00:11 +0300

    added logging to error handling

--- project/backend/src/error.js
+++ project/backend/src/error.js
@@ -1,5 +1,6 @@
 const handleError = (error, request, response, next) => {
     response.status(500);
+    console.error(error);
     return response.json({
         message: error.message,
         stack: process.env.NODE_ENV == "production" ? null : error.stack,
